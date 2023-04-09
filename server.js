982b6bce60e47d277b7bac334b45986b8c442c45
Josephsomethingelse12341 <joseph.degois17@gmail.com>
2023-04-09 02:16:08 -0400

    as

---
+++ server.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express')
+
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+
+mongoose.connect('mongodb+srv://Josephadmin:7HYpxGTzZiOnEocy@cluster0.gsshbsu.mongodb.net/comp3133_assignment1?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(success => {
+  console.log('Success Mongodb connection')
+})
+.catch(err => {
+  console.log('Error Mongodb connection')
+});
+
+async function startApolloServer(typeDefs, resolvers){
+    const server = new ApolloServer({typeDefs, resolvers})
+    const app = express();
+    await server.start();
+    server.applyMiddleware({app, path: '/graphql'});
+    
+    app.listen({ port: 4000 }, () => {
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    });
+}
+
+
+startApolloServer(typeDefs, resolvers);
+
+
+
